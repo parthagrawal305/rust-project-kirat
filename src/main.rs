8e1a039b9214fa65b4cd7d5c75e281e33f4fc6b1
Parth Agrawal <parthagrawal2183@gmail.com>
2024-05-26 15:50:21 +0530

    debuged nth pos error

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let greeting: String = String::from("hello world");
+    println!("{}", greeting);
+
+    let n: usize = 13;
+    let char1 = greeting.chars().nth(n);
+
+    match char1 {
+        Some(c) => println!("The character at {}th postition is: {}", n, c),
+        None => println!("No character found at index {}", n),
+    }
 }
