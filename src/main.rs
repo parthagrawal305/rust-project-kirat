f36f5fc8015876eaaceeda53fd0d180ed41a18ce
Parth Agrawal <parthagrawal2183@gmail.com>
2024-05-26 15:56:47 +0530

    conditional statements

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 fn main() {
-    let greeting: String = String::from("hello world");
-    println!("{}", greeting);
-
-    let n: usize = 13;
-    let char1 = greeting.chars().nth(n);
-
-    match char1 {
-        Some(c) => println!("The character at {}th postition is: {}", n, c),
-        None => println!("No character found at index {}", n),
+    let is_even = true;
+    if is_even{
+        println!("Even");
+    } else {
+        println!("Odd");
     }
+
 }
