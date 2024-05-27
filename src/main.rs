8b4340af4fde8559d3019176e920500d7d7c2a70
Parth Agrawal <parthagrawal2183@gmail.com>
2024-05-27 12:11:32 +0530

    functions

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 fn main() {
-    let is_even = true;
-    if is_even{
-        println!("Even");
-    } else {
-        println!("Odd");
-    }
+    let sentence = String::from("My name is Parth.");
+    let first_word = get_first_word(sentence);
+    // for i in 0..10000000 {
+    //     println!("Hello, World!{}", i);
+    // }
+    println!("First word is: {}", first_word);
+}
 
+fn get_first_word(sentence: String) -> String {
+    let mut ans = String::from("");
+    for char in sentence.chars() {
+        if char == ' ' {
+            break;
+        }
+        ans.push_str(char.to_string().as_str());
+    }
+    return ans;
 }
